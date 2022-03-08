5765042d80aeebe84647bdac9a4f8cfe3466abf1
nitrotap <kartikinpublic@gmail.com>
2022-03-08 16:00:59 -0700

    fixing type in server at line 50

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/notes', (req, res) => {
 	res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/index.html'));
+	res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 function createNewNote(body, notes) {
